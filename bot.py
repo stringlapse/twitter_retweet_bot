0cdcef5296170a486abc5818f1375693c2c7aabb
Ryder Sadlovsky <24516915+stringlapse@users.noreply.github.com>
2019-10-14 13:43:30 -0600

    Added logging stuff

--- bot.py
+++ bot.py
@@ -7,12 +7,15 @@ api = secrets.login()
 mentions = api.mentions_timeline(count=5)
 
 for mention in mentions:
+    print("[NEW ] Found mention with Tweet id " + mention.id)
+
+for mention in mentions:
     if mention.in_reply_to_user_id == bot_user_id:
-        print("Tweet id " + str(mention.id) + " is a reply to us")
+        print("[PASS] Tweet id " + str(mention.id) + " is a reply to us")
         continue
     try:
         api.retweet(mention.id)
     except:
-        print("We already Retweeted Tweet id " + str(mention.id))
+        print("[FAIL] We already Retweeted Tweet id " + str(mention.id))
         continue
-    print("Successfully Retweeted Tweet id " + str(mention.id))
+    print("[ RT ] Successfully Retweeted Tweet id " + str(mention.id))
