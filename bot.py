ec528ec77190286851ae57e9b936d170d61aa628
stringlapse <24516915+stringlapse@users.noreply.github.com>
2020-01-31 22:29:49 -0700

    Update bot.py
    Bot now also checks for two different triggers in the text (ex. username and hashtag)

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import tweepy, secrets
 
 bot_user_id = XXXXX
+rt_trigger_1 = "XXXXX"
+rt_trigger_2 = "XXXXX"
 
 api = secrets.login()
 
@@ -12,6 +14,9 @@ for mention in mentions:
 for mention in mentions:
     if mention.in_reply_to_user_id == bot_user_id:
         print("[PASS] Tweet id " + str(mention.id) + " is a reply to us")
+        continue
+    if not (rt_trigger_1 in mention.full_text.lower() and rt_trigger_2 in mention.full_text.lower()):
+        print("[PASS] Tweet id " + str(mention.id) + " is  missing triggers")
         continue
     try:
         api.retweet(mention.id)
